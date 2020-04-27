9dc17772dddc75e249d6545d1e09ec8af9c9d7a9
Rob Walch <rob@jwplayer.com>
2020-04-27 19:23:44 -0400

    Only output js files and modules needed by headless player JW8-10813

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -77,11 +77,11 @@ Object.assign(Controller.prototype, {
 
         const _backgroundLoading = _model.get('backgroundLoading');
 
-        const viewModel = new ViewModel(_model);
-
         if (__HEADLESS__) {
             _model.attributes.visibility = 1.0;
         } else {
+            const viewModel = new ViewModel(_model);
+
             _view = this._view = new View(_api, viewModel);
             _view.on('all', (type, event) => {
                 if (event && event.doNotForward) {
@@ -89,6 +89,10 @@ Object.assign(Controller.prototype, {
                 }
                 _trigger(type, event);
             }, _this);
+
+            viewModel.on('viewSetup', (viewElement) => {
+                showView(this, viewElement);
+            });
         }
 
         const _programController = this._programController = new ProgramController(_model, mediaPool, _api._publicApi);
@@ -214,12 +218,10 @@ Object.assign(Controller.prototype, {
         });
 
         // Ensure captionsList event is raised after playlistItem
-        _captions = new Captions(_model);
-        _captions.on('all', _trigger, _this);
-
-        viewModel.on('viewSetup', (viewElement) => {
-            showView(this, viewElement);
-        });
+        if (!__HEADLESS__) {
+            _captions = new Captions(_model);
+            _captions.on('all', _trigger, _this);
+        }
 
         this.playerReady = function() {
             if (__HEADLESS__) {
@@ -347,7 +349,7 @@ Object.assign(Controller.prototype, {
             if (_model.get('state') === 'idle' && _model.get('autostart') === false) {
                 // If video has not been primed on Android, test that video will play before preloading
                 // This ensures we always prime the tag on play when necessary
-                if (!mediaPool.primed() && OS.android) {
+                if (!__HEADLESS__ && !mediaPool.primed() && OS.android) {
                     const video = mediaPool.getTestElement();
                     const muted = _this.getMute();
                     Promise.resolve().then(() => startPlayback(video, { muted })).then(() => {
@@ -579,7 +581,8 @@ Object.assign(Controller.prototype, {
 
             // Detect and store browser autoplay setting in the model.
             const adConfig = _model.get('advertising');
-            canAutoplay(mediaPool, {
+            const autoPlayCheck = __HEADLESS__ ? Promise.resolve.bind(Promise) : canAutoplay;
+            autoPlayCheck(mediaPool, {
                 cancelable: checkAutoStartCancelable,
                 muted: _this.getMute(),
                 allowMuted: adConfig ? adConfig.autoplayadsmuted : true
@@ -587,7 +590,7 @@ Object.assign(Controller.prototype, {
                 _model.set('canAutoplay', result);
 
                 // Only apply autostartMuted on un-muted autostart attempt.
-                if (result === AUTOPLAY_MUTED && !_this.getMute()) {
+                if (result === (__HEADLESS__ || AUTOPLAY_MUTED) && !_this.getMute()) {
                     _model.set('autostartMuted', true);
                     updateProgramSoundSettings();
 
@@ -597,7 +600,7 @@ Object.assign(Controller.prototype, {
                     });
                 }
 
-                if (_this.getMute() && _model.get('enableDefaultCaptions')) {
+                if (_captions && _this.getMute() && _model.get('enableDefaultCaptions')) {
                     _captions.selectDefaultIndex(1);
                 }
 
@@ -608,7 +611,7 @@ Object.assign(Controller.prototype, {
                     _actionOnAttach = null;
                 });
             }).catch(error => {
-                _model.set('canAutoplay', AUTOPLAY_DISABLED);
+                _model.set('canAutoplay', (__HEADLESS__ || AUTOPLAY_DISABLED));
                 _model.set('autostart', false);
                 // Emit event unless test was explicitly canceled.
                 if (!checkAutoStartCancelable.cancelled()) {
@@ -816,11 +819,11 @@ Object.assign(Controller.prototype, {
         }
 
         function _getCurrentCaptions() {
-            return _captions.getCurrentIndex();
+            return _captions ? _captions.getCurrentIndex() : -1;
         }
 
         function _getCaptionsList() {
-            return _captions.getCaptionsList();
+            return _captions ? _captions.getCaptionsList() : [];
         }
 
         /* Used for the InStream API */
@@ -859,6 +862,8 @@ Object.assign(Controller.prototype, {
                 state = !_model.get('fullscreen');
             }
 
+            // TODO: rather than the player responding to this state change this should be view / provider based with
+            //  Promise resolution for modern browsers
             _model.set('fullscreen', state);
             if (_this._instreamAdapter && _this._instreamAdapter._adModel) {
                 _this._instreamAdapter._adModel.set('fullscreen', state);
@@ -869,6 +874,9 @@ Object.assign(Controller.prototype, {
             _programController
                 .on('all', _trigger, _this)
                 .on('subtitlesTracks', (e) => {
+                    if (!_captions) {
+                        return;
+                    }
                     _captions.setSubtitlesTracks(e.tracks);
                     const defaultCaptionsIndex = _captions.getCurrentIndex();
 
@@ -1033,7 +1041,14 @@ Object.assign(Controller.prototype, {
 
         // View passthroughs
         if (__HEADLESS__) {
-            this.resize = this.getSafeRegion = this.setCaptions = function() {};
+            // These should be overridden by the app using the headless player
+            this.resize = this.setCaptions = function() {};
+            this.getSafeRegion = () => ({
+                x: 0,
+                y: 0,
+                width: 0,
+                height: 0
+            });
         } else {
             this.resize = _view.resize;
             this.getSafeRegion = _view.getSafeRegion;
@@ -1045,6 +1060,9 @@ Object.assign(Controller.prototype, {
         };
 
         this.setControls = function (mode) {
+            if (__HEADLESS__) {
+                return;
+            }
             if (!isBoolean(mode)) {
                 mode = !_model.get('controls');
             }
